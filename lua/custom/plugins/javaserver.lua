992b5e2955785362416d6ad3c6a2aa01ebb566ee
JACOBIN-SCTCS <jacobja3@gmail.com>
2025-09-23 23:20:32 +0530

    Testing on windows

--- lua/custom/plugins/javaserver.lua
+++ lua/custom/plugins/javaserver.lua
@@ -3,154 +3,180 @@ local M = {}
 local tomcat_job_id = nil
 local buffer_id = nil
 
-M.tomcatdirectory = '/Users/depressedcoder/Downloads/apache-tomcat-10.1.44/'
+-- windows equivalent of listing  dir /a /b
+--M.tomcatdirectory = "/Users/depressedcoder/Downloads/apache-tomcat-10.1.44/"
 -- Identifying the OS : print(vim.loop.os_uname().sysname)
 
+M.tomcatdirectory = "C:\\Users\\X\\Downloads\\apache-tomcat-10.1.46\\apache-tomcat-10.1.46"
+
 local function validateDefaultTomcatApps(app_name)
-  local defaultApps = { 'docs', 'examples', 'host-manager', 'manager', 'ROOT' }
-  for index, app in ipairs(defaultApps) do
-    if app == app_name then
-      return true
-    end
-  end
-  return false
+	local defaultApps = { "docs", "examples", "host-manager", "manager", "ROOT" }
+	for index, app in ipairs(defaultApps) do
+		if app == app_name then
+			return true
+		end
+	end
+	return false
 end
 
 local function scanTomcatWebAppsFolder()
-  local i, t, popen = 0, {}, io.popen
-  local pfile = popen('ls -a -d -1 "' .. M.tomcatdirectory .. 'webapps/"**')
-  if pfile ~= nil then
-    for filename in pfile:lines() do
-      i = i + 1
-      t[i] = filename
-    end
-    pfile:close()
-  end
-  return t
+	local i, t, popen = 0, {}, io.popen
+	local pfile = nil
+
+	if vim.loop.os_uname().sysname == "Windows_NT" then
+		pfile = popen("dir /a /b " .. M.tomcatdirectory .. "/webapps/")
+	else
+		pfile = popen('ls -a -d -1 "' .. M.tomcatdirectory .. '/webapps/"**')
+	end
+
+	--local pfile = popen('ls -a -d -1 "' .. M.tomcatdirectory .. 'webapps/"**')
+	if pfile ~= nil then
+		for filename in pfile:lines() do
+			i = i + 1
+			t[i] = filename
+		end
+		pfile:close()
+	end
+	return t
 end
 
 function M.cleanWebAppsFolder()
-  local folders = scanTomcatWebAppsFolder()
-  for _, folder in ipairs(folders) do
-    local foldername = vim.fn.fnamemodify(folder, ':t')
-    if not validateDefaultTomcatApps(foldername) then
-      vim.fs.rm(folder, {
-        recursive = true,
-      })
-    end
-  end
+	local folders = scanTomcatWebAppsFolder()
+	for _, folder in ipairs(folders) do
+		local foldername = vim.fn.fnamemodify(folder, ":t")
+		if not validateDefaultTomcatApps(foldername) then
+			vim.fs.rm(folder, {
+				recursive = true,
+			})
+		end
+	end
 end
 
 function M.startTomcat(debug)
-  --local bufnr = vim.api.nvim_win_call()
+	--local bufnr = vim.api.nvim_win_call()
 
-  debug = debug or false
+	debug = debug or false
 
-  local runcommand = nil
+	local runcommand = nil
+	local script_suffix = ".sh"
 
-  if debug then
-    runcommand = 'bash ' .. M.tomcatdirectory .. 'bin/catalina.sh jpda run'
-  else
-    runcommand = 'bash ' .. M.tomcatdirectory .. 'bin/catalina.sh run'
-  end
+	if vim.loop.os_uname().sysname == "Windows_NT" then
+		script_suffix = ".bat"
+	else
+		script_suffix = ".sh"
+	end
 
-  --print(runcommand)
-  if tomcat_job_id == nil then
-    local buf = nil
-    if buffer_id ~= nil and vim.api.nvim_buf_is_loaded(buffer_id) then
-      buf = buffer_id
-    else
-      buf = vim.api.nvim_create_buf(true, true)
-      buffer_id = buf
-    end
-    vim.api.nvim_buf_set_name(buf, 'Tomcat Server Logs [' .. buf .. ']')
+	if debug then
+		runcommand = "bash " .. M.tomcatdirectory .. "bin/catalina" .. script_suffix .. " jpda run"
+	else
+		runcommand = "bash " .. M.tomcatdirectory .. "bin/catalina" .. script_suffix .. " run"
+	end
 
-    --vim.bo[buf].buftype = 'nofile'
-    --vim.bo[buf].bufhidden = 'hide'
-    --vim.bo[buf].swapfile = false
+	--print(runcommand)
+	if tomcat_job_id == nil then
+		local buf = nil
+		if buffer_id ~= nil and vim.api.nvim_buf_is_loaded(buffer_id) then
+			buf = buffer_id
+		else
+			buf = vim.api.nvim_create_buf(true, true)
+			buffer_id = buf
+		end
+		vim.api.nvim_buf_set_name(buf, "Tomcat Server Logs [" .. buf .. "]")
 
-    vim.api.nvim_set_current_buf(buf)
-    vim.api.nvim_buf_call(buf, function()
-      tomcat_job_id = vim.fn.jobstart(runcommand, {
+		--vim.bo[buf].buftype = 'nofile'
+		--vim.bo[buf].bufhidden = 'hide'
+		--vim.bo[buf].swapfile = false
 
-        on_stdout = function(_, data, _)
-          if data then
-            vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
-            vim.api.nvim_command 'normal! G'
-          end
-        end,
-        on_stderr = function(_, data, _)
-          if data then
-            vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
-            vim.api.nvim_command 'normal! G'
-          end
-        end,
-      })
-    end)
-    vim.api.nvim_buf_attach(buf, false, {
-      on_detach = function()
-        M.stopTomcat()
-      end,
-    })
-  else
-    if buffer_id ~= nil and vim.api.nvim_buf_is_loaded(buffer_id) then
-      vim.api.nvim_set_current_buf(buffer_id)
-    end
-  end
+		vim.api.nvim_set_current_buf(buf)
+		vim.api.nvim_buf_call(buf, function()
+			tomcat_job_id = vim.fn.jobstart(runcommand, {
+
+				on_stdout = function(_, data, _)
+					if data then
+						vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+						vim.api.nvim_command("normal! G")
+					end
+				end,
+				on_stderr = function(_, data, _)
+					if data then
+						vim.api.nvim_buf_set_lines(buf, -1, -1, false, data)
+						vim.api.nvim_command("normal! G")
+					end
+				end,
+			})
+		end)
+		vim.api.nvim_buf_attach(buf, false, {
+			on_detach = function()
+				M.stopTomcat()
+			end,
+		})
+	else
+		if buffer_id ~= nil and vim.api.nvim_buf_is_loaded(buffer_id) then
+			vim.api.nvim_set_current_buf(buffer_id)
+		end
+	end
 end
 
 function M.stopTomcat()
-  if tomcat_job_id ~= nil then
-    vim.fn.jobstop(tomcat_job_id)
-    vim.fn.jobwait({ tomcat_job_id }, 100)
-    tomcat_job_id = nil
-    M.cleanWebAppsFolder()
-    -- print 'Tomcat Stopped'
-  end
+	if tomcat_job_id ~= nil then
+		vim.fn.jobstop(tomcat_job_id)
+		vim.fn.jobwait({ tomcat_job_id }, 100)
+		tomcat_job_id = nil
+		M.cleanWebAppsFolder()
+		-- print 'Tomcat Stopped'
+	end
 end
 
 function M.create_war(runWar)
-  runWar = runWar or false
+	runWar = runWar or false
 
-  if vim.fn.isdirectory(vim.fn.getcwd() .. '/' .. 'WebContent') and vim.fn.isdirectory(vim.fn.getcwd() .. '/' .. 'build') then
-    local war_file_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t') .. '.war'
+	local command = ""
+	local move_command = nil
+	local war_file_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t") .. ".war"
 
-    vim.fn.jobstart(
-      'cp -r build/ WebContent/WEB-INF ; cp -r src WebContent/WEB-INF ; jar -cvf '
-        .. war_file_name
-        .. ' -C WebContent/ .; rm -rf WebContent/WEB-INF/classes; rm -rf WebContent/WEB-INF/src',
-      {
-        on_exit = function(_, code, _)
-          -- print('Create War Exited : Code ' .. code)
-          if runWar and vim.uv.fs_stat(war_file_name) then
-            os.execute('mv ' .. war_file_name .. ' ' .. M.tomcatdirectory .. 'webapps/')
-            if buffer_id ~= nil then
-              vim.api.nvim_set_current_buf(buffer_id)
-            end
-          end
-        end,
-      }
-    )
-    return war_file_name
-  end
+	if vim.loop.os_uname().sysname == "Windows_NT" then
+	else
+		command = "cp -r build/ WebContent/WEB-INF ; cp -r src WebContent/WEB-INF ; jar -cvf "
+			.. war_file_name
+			.. " -C WebContent/ .; rm -rf WebContent/WEB-INF/classes; rm -rf WebContent/WEB-INF/src"
+		move_command = "mv " .. war_file_name .. " " .. M.tomcatdirectory .. "webapps/"
+	end
+
+	if
+		vim.fn.isdirectory(vim.fn.getcwd() .. "/" .. "WebContent")
+		and vim.fn.isdirectory(vim.fn.getcwd() .. "/" .. "build")
+	then
+		vim.fn.jobstart(command, {
+			on_exit = function(_, code, _)
+				-- print('Create War Exited : Code ' .. code)
+				if runWar and vim.uv.fs_stat(war_file_name) then
+					os.execute(move_command)
+					if buffer_id ~= nil then
+						vim.api.nvim_set_current_buf(buffer_id)
+					end
+				end
+			end,
+		})
+		return war_file_name
+	end
 end
 
 function M.run_project()
-  M.stopTomcat()
-  M.startTomcat()
-  M.create_war {
-    runWar = true,
-  }
+	M.stopTomcat()
+	M.startTomcat()
+	M.create_war({
+		runWar = true,
+	})
 end
 
 function M.debug_project()
-  M.stopTomcat()
-  M.startTomcat {
-    debug = true,
-  }
-  M.create_war {
-    runWar = true,
-  }
+	M.stopTomcat()
+	M.startTomcat({
+		debug = true,
+	})
+	M.create_war({
+		runWar = true,
+	})
 end
 
 return M
