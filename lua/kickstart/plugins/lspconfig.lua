47cf14c7e897a35468aa55e8e03a1041a79fccca
jacob <jacobja3@gmail.com>
2025-10-19 17:21:00 +0530

    Added the plugin trouble.nvim , changed python lanugage server, added clangd, formatting for java files

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -212,7 +212,9 @@ return {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {
+          mason = false,
+        },
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -229,7 +231,9 @@ return {
         --jdtls = {},
         --java_test = {},
         --java_debug_adapter = {},
-        pyright = {},
+        --pyright = {},
+        --python_lsp_server = {},
+        pylsp = {},
         html = {},
         cssls = {},
         lemminx = {},
